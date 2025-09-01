d05925f85182f5bd3654b1dbbe4bc7cbf2d945a2
fnnln <mail@fnnln.com>
2025-09-01 20:43:34 +0200

    fix: :bug: Fix imports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // Reexport your entry components here
 import Form from './autoform.svelte';
-import { field_registry, form_registry } from './zod_adapter.ts';
+import { field_registry, form_registry } from './zod_adapter.js';
 import type { AutoFormState } from './autoformstate.svelte.js';
 import type { FormMeta, FieldMeta } from './types.d.js';
 
